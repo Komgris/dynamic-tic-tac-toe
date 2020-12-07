6607684b9de2e1d0e2fdb609327f211074c5e32b
devspsec <devs.psec@gmail.com>
2020-12-08 06:48:11 +0700

    add btn n input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,11 +12,17 @@ export class AppComponent implements OnInit {
   header:any = [];
   x= true;
   o = false;
-  sizenumber = 5;
+  sizenumber = 3;
+  human = true;
+  computer = false;
 
   ngOnInit() {
-    this.table = this.createTable(this.sizenumber);
-    this.header = Object.keys(this.createTable(this.sizenumber));
+    this.renderTable(this.sizenumber)
+  }
+
+  renderTable(number:number){
+    this.table = this.createTable(number);
+    this.header = Object.keys(this.createTable(number));
   }
   createTable(number:number){
     let col=[]
@@ -38,19 +44,45 @@ export class AppComponent implements OnInit {
 
   turn(row:number,col:number){
     this.table[row][+col][+col]=true;
-    if(this.winpattern(true)){
+
+    if(this.winpattern(this.table,true)){
       console.log('win!!')
     }
+    console.log(this.emptySpace());
   }
 
-  winpattern(side:boolean){
+  emptySpace(){
+
+  }
+
+  demoMove(side:boolean){
+    let col=[];
+    let point = 0;
+    let demotable = this.table;
+    for(let i =0; i<this.sizenumber;i++){
+      let row:any=[];
+      for(let j =0; j<this.sizenumber;j++){
+        if(demotable[i][j][j] === 0){
+          demotable[i][j][j] === side
+          if(this.winpattern(demotable,this.computer)){
+            point++; 
+          }
+        }
+      }
+      col.push(row);
+    }
+    return col;
+
+  }
+
+  winpattern(table:any,side:boolean){
     let isWin=false;
     let isWinArray:any =[];
 
     //horizontal
     this.header.map((x: string | number)=>{
       this.header.map((y: string | number)=>{
-        isWinArray.push( this.table[x][y][y]);
+        isWinArray.push( table[x][y][y]);
       })
       
       if((isWinArray.filter((res: boolean)=>res==!side)).length === 0){
@@ -62,7 +94,7 @@ export class AppComponent implements OnInit {
     //vertical
     this.header.map((x: string | number)=>{
       this.header.map((y: string | number)=>{
-        isWinArray.push( this.table[y][x][x]);
+        isWinArray.push( table[y][x][x]);
       })
       
       if((isWinArray.filter((res: boolean)=>res==!side)).length === 0){
@@ -74,7 +106,7 @@ export class AppComponent implements OnInit {
 
     //diagonal 
       for(let x = 0; x<this.sizenumber;x++){
-        isWinArray.push(this.table[x][x][x]);
+        isWinArray.push(table[x][x][x]);
       }
       if((isWinArray.filter((res: boolean)=>res==!side)).length === 0){
         isWin = true;
@@ -84,7 +116,7 @@ export class AppComponent implements OnInit {
     //diagonal 
       let y = this.sizenumber-1;
       for(let x = 0; x<this.sizenumber;x++){     
-        isWinArray.push(this.table[y-x][x][x]);
+        isWinArray.push(table[y-x][x][x]);
       }
       if((isWinArray.filter((res: boolean)=>res==!side)).length === 0){
         isWin = true;
